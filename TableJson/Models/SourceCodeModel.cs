720ed9d93c3df793b82c024bf5f8efe9fd7e42cf
Andrew Malyshev <69252550+Wowhere@users.noreply.github.com>
2025-01-03 14:38:10 +0300

    Add code model and update deps

---
+++ TableJson/Models/SourceCodeModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TableJson.Models
+{
+    public enum MacrosTypes
+    {
+        RightClick = 1,
+        Manually = 2,
+        Combined = 3,
+    }
+    public class Macros
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string? Description { get; set; }
+        public MacrosTypes MacrosType { get; set; }
+        public string? SourceCode { get; set; } = "";
+        public byte[]? ActionTreeExpression { get; set; }
+        [NotMapped]
+        public bool IsSaved { get; set; } = true; //false = temp, true = in DB
+        public Macros(bool isSaved)
+        {
+            IsSaved = isSaved;
+        }
+        public Macros(int id, bool isActive, string description, string command)
+        {
+            Id = id;
+            IsActive = isActive;
+            Description = description;
+        }
+    }
+    public class HelpContext : DbContext
+    {
+        public DbSet<Macros>? VoiceOperationTable { get; set; }
+        private string DbPath { get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+        public HelpContext()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            DbPath = System.IO.Path.Join(path, "jsonscript.db");
+        }
+    }
+}
