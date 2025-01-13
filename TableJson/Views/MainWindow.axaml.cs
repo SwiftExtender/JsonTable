c038c82af4691b5145dabed4e58a7e57fa02091c
Andrew Malyshev <69252550+Wowhere@users.noreply.github.com>
2025-01-13 17:15:01 +0300

    Many improvements

--- TableJson/Views/MainWindow.axaml.cs
+++ TableJson/Views/MainWindow.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using AvaloniaEdit;
 using System;
+using System.IO;
 using TableJson.ViewModels;
 
 namespace TableJson.Views
@@ -36,6 +38,39 @@ namespace TableJson.Views
             if (focused != null)
             {
                 focused.Loaded += (s, e) => focused.Focus();
+            }
+        }
+        public async void CopyToClipboardFromListbox(object sender, TappedEventArgs e)
+        {
+            if (e.Source.GetType() == typeof(ListBox))
+            {
+                TextBox c = (TextBox)sender;
+                var clipboard = GetTopLevel(c).Clipboard;
+                var dataObject = new DataObject();
+
+                dataObject.Set(DataFormats.Text, ((TextBlock)e.Source).Text);
+                await clipboard.SetDataObjectAsync(dataObject);
+            }
+        }
+        private async void ImportJsonFile_Clicked(object sender, RoutedEventArgs args)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open File with JSON",
+                AllowMultiple = false
+            });
+
+            if (files.Count >= 1)
+            {
+                // Open reading stream from the first file.
+                await using var stream = await files[0].OpenReadAsync();
+                using var streamReader = new StreamReader(stream);
+                // Reads all the content of file as a text.
+                var fileContent = await streamReader.ReadToEndAsync();
             }
         }
     }
